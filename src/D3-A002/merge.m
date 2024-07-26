32062c40802a10f9f514297cebaff5e24cdd8aff
bud42 <bdboyd42@gmail.com>
2024-07-26 11:36:07 -0400

    Create merge.m

---
+++ src/D3-A002/merge.m
@@ -0,0 +1,23 @@
+% Merges all conn.mat files into a single conn.mat
+
+% Merged mat file to save
+fileoutput='/tmp/DrSarah_CONN/conn.mat';
+
+% Load all conn.mat in /INPUTS
+dirfiles = dir('/INPUTS/**/conn.mat')
+
+% Get a list of full paths
+fileinputs = fullfile({dirfiles.folder}, {dirfiles.name});
+
+% Load first file and save to initialize
+conn('load', fileinputs{1});
+conn('save', fileoutput);
+
+% Merge all
+conn_merge(char(fileinputs), [], true, true);
+
+% Run the postmerge step to verify
+conn_process postmerge;
+
+% Save the merged
+conn('save', fileoutput);
