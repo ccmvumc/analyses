0ee072c3db2eaacbe54bef15af08ced701a6775f
Jason Russell <jason.russell34@gmail.com>
2024-08-19 16:38:32 -0500

    Initial commit of centiloid pipeline for validation.
    Atlases added to github atlases folder, need to index these
    accurately. Once validated need to update pipeline to output
    CLs based on established formula

---
+++ src/GAAIN-A001/centiloid_suvr_extraction.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import os
+import ants
+from nilearn import image, masking
+import nibabel as nib
+import pandas as pd
+import glob
+
+in_dir = '/INPUTS'
+###SAVED IN THE 'atlases' FOLDER ON GITHUB
+atlas = '/spm8_MNI_avg152T1.nii'
+out_dir = '/OUTPUTS/DATA'
+
+
+os.makedirs(out_dir)
+
+#Open atlas
+mni = ants.image_read(atlas)
+
+# Import gaain masks ###SAVED IN THE 'atlases' FOLDER ON GITHUB
+ctx_voi = image.load_img(
+	'centiloid_ctx_2mm.nii'
+	)
+
+wcbm_voi = image.load_img(
+	'/centiloid_WhlCbl_2mm.nii'
+	)
+
+
+fixed = mni
+
+output_df = pd.DataFrame(columns=['ID', 'SUVR'])
+
+for subject in sorted(os.listdir(in_dir)):
+	if subject.startswith('.'):
+		# ignore hidden files and other junk
+		continue
+
+	subject_amyloid = glob.glob(f'{in_dir}/{subject}/*PiB*')
+	subject_mr = glob.glob(f'{in_dir}/{subject}/*MR*')
+
+	print('Amyloid:', subject_amyloid)
+
+	subject_out = f'{out_dir}/{subject}'
+
+	os.makedirs(subject_out)
+
+	# Get full file path to input images
+	orig_file = subject_mr
+	amyloid_file = subject_amyloid
+	
+	# Skull Strip Original T1 and mask
+	raw = ants.image_read(orig_file)
+	raw_pet = ants.image_read(amyloid_file)
+	#mri_strip_mask = brain_extraction(raw, modality='t1')
+	
+	#print orientation of PET and MRI
+	pet_img = nib.load(subject_amyloid)
+	mr_img = nib.load(subject_mr)
+	
+	current_ornt_pet = nib.aff2axcodes(pet_img.affine)
+	current_ornt_mri = nib.aff2axcodes(mr_img.affine)
+	
+	print(f'PET image orientation: {current_ornt_pet}')
+	print(f'MRI image orientation: {current_ornt_mri}')
+	
+	# Load orig T1 image as moving image for registration
+	moving = raw
+	
+	#warp PET to MR
+	warp_pet = ants.registration(moving, raw_pet, type_of_transform='SyN')
+
+	# Do Registration of Moving to Fixed
+	reg = ants.registration(fixed, moving, type_of_transform='SyN')
+
+	# Save warped orig
+	warped_orig_file = f'{subject_out}/warped_orig.nii.gz'
+	ants.image_write(reg['warpedmovout'], warped_orig_file)
+
+	# Apply transform to amyloid image which is already in same space
+	warped_amyloid_file = f'{subject_out}/warped_amyloid.nii.gz'
+	amyloid = warp_pet['warpedmovout']
+	warped_amyloid = ants.apply_transforms(fixed, amyloid, reg['fwdtransforms'])
+	ants.image_write(warped_amyloid, warped_amyloid_file)
+	
+	#import image as nifti1
+	xformed_PET = image.load_img(f'{subject_out}/warped_amyloid.nii.gz')
+	
+	resampled_PET = image.resample_to_img(xformed_PET, ctx_voi)
+	
+	#apply masks
+	ctx_masked = masking.apply_mask(resampled_PET, ctx_voi)
+	wcbm_masked = masking.apply_mask(resampled_PET, wcbm_voi)
+	
+	# Calculate the mean uptake in the VOIs
+	mean_uptake_voi = ctx_masked.mean()
+	mean_uptake_ref = wcbm_masked.mean()
+	
+	suvr = mean_uptake_voi/mean_uptake_ref
+	
+	row = [subject, suvr]
+	
+	output_df.loc[len(output_df)] = row
+	
+output_df.to_csv(f'{out_dir}/standard_centiloid_suvrs.csv', index=False)
+
+
+
---
+++ src/GAAIN-A001/main.sh
@@ -0,0 +1,3 @@
+
+python /REPO/src/GAAIN-A001/centiloid_suvr_extraction.py
+python /REPO/src/GAAIN-A001/PiB_validation.py
