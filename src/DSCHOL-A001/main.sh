25c1e6096ab6430308994f1d04f02f083bfd5bf1
Jason Russell <jason.russell34@gmail.com>
2024-08-23 10:35:14 -0500

    First commit of centiloid glm pipeline

---
+++ src/DSCHOL-A001/main.sh
@@ -0,0 +1,5 @@
+
+python /REPO/src/DSCHOL-A006/normalize.py
+python /REPO/src/DSCHOL-A006/indiv_masks.py
+python /REPO/src/DSCHOL-A006/study_specific_gm_mask.py
+python /REPO/src/DSCHOL-A006/voxelwise_centiloid_glm.py
