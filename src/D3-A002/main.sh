fe260e861afd3fd6499b07d6744d9b02e0cc0c1d
bud42 <bdboyd42@gmail.com>
2024-08-23 18:19:57 -0400

    updates to load covars

--- src/D3-A002/main.sh
+++ src/D3-A002/main.sh
@@ -11,6 +11,15 @@ xvfb-run \
 /opt/mcr/v912 batch \
 /REPO/src/D3-A002/merge.m
 
+# covars
+echo "Loading covariates to CONN project"
+xvfb-run \
+-e /OUTPUTS/xvfb.err -f /OUTPUTS/xvfb.auth \
+-a --server-args "-screen 0 1600x1200x24" \
+/opt/conn/run_conn.sh \
+/opt/mcr/v912 batch \
+/REPO/src/D3-A002/load_covariates.m
+
 rm /OUTPUTS/xvfb.auth /OUTPUTS/xvfb.err
 
 echo "ALL DONE!"
