a5eade5976c25e73bb1e2b0e2b1b331cb136cebf
bud42 <bdboyd42@gmail.com>
2024-11-04 11:39:46 -0500

    add conn post

---
+++ src/CONN/merge-post.sh
@@ -0,0 +1,6 @@
+rm /OUTPUTS/xvfb.auth
+rm /OUTPUTS/xvfb.err
+
+# Delete merged pre-processed files since we only want 1st Level Results
+rm -r /OUTPUTS/conn/data
+rm -r /OUTPUTS/conn/results/preprocessing
