aca0c7e9c6c3a1a9fe15961f0336d0e8b0456b9e
bud42 <bdboyd42@gmail.com>
2024-11-05 13:30:27 -0500

    update CONN covar loading

--- src/CONN/covariate_csv2mat.py
+++ src/CONN/covariate_csv2mat.py
@@ -9,17 +9,31 @@ CSVFILE = '/INPUTS/covariates.csv'
 MATFILE = '/OUTPUTS/covariates.mat'
 mat = {}
 
+# Load subject data
 df = pd.read_csv(CSVFILE)
+
+# Mean center age
+df['AGE'] = df['AGE'].astype(float)
+df['AGE'] = (df['AGE'] - df['AGE'].mean()) / df['AGE'].std()
+
+# Convert categorical columns to dummy columns
+dummies = ['SEX', 'GROUP']
+if 'SITE' in df.columns:
+    dummies.append('SITE')
+
+df = pd.get_dummies(df, columns=dummies, dtype='int')
 
 # Filter out non-numeric data and force float
 effects = df.select_dtypes(include=[np.number])
 effects = effects.astype(float)
-names = effects.columns
+names = [x for x in effects.columns if x != 'ID']
 
+# Build vector of values for each covariate
 np_vectors = np.empty((len(names),), dtype=object)
 for i in range(len(names)):
     np_vectors[i] = np.transpose(np.array([effects[names[i]]]))
 
+# Store in mat format as expected by SPM/CONN
 mat['effect_names'] = np.array(names, dtype=object)
 mat['effects'] =  np_vectors
 
--- src/CONN/merge-post.sh
+++ src/CONN/merge-post.sh
@@ -2,7 +2,7 @@ rm /OUTPUTS/xvfb.auth
 rm /OUTPUTS/xvfb.err
 
 # Delete merged pre-processed files since we only want 1st Level Results
-rm -r /OUTPUTS/conn/results/preprocessing/*.mat
+rm -r /OUTPUTS/conn/results/preprocessing/[A-Za-z]*.mat
 rm -r /OUTPUTS/conn/results/preprocessing/*.nii
 rm -r /OUTPUTS/conn/results/preprocessing/*.matc
 rm -r /OUTPUTS/conn/data/*.mat
