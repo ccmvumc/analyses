9c1f9b73be6b66aa959d9e9015a68f6852af5d95
bud42 <bdboyd42@gmail.com>
2024-12-05 10:29:22 -0500

    Update zvalues_csv2csv.py

--- src/CONN/zvalues_csv2csv.py
+++ src/CONN/zvalues_csv2csv.py
@@ -5,18 +5,23 @@ ZFILE = '/OUTPUTS/zvalues.csv'
 
 
 def get_names(df):
+    # Get atlas name before first period
     df['r1atlas'] = df['r1name'].str.extract(r'([^.]*)')
     df['r2atlas'] = df['r2name'].str.extract(r'([^.]*)')
 
+    # hemisphere is after period, between two underscores
     df['r1hemi'] = df['r1name'].str.extract(r'\.[^_]+_([^_]+)_')
     df['r2hemi'] = df['r2name'].str.extract(r'\.[^_]+_([^_]+)_')
-
+    
+    # network is after period, between 2nd and 3rd underscores
     df['r1network'] = df['r1name'].str.extract(r'\.[^_]*_(?:[^_]*_){1}([^_]+)_')
     df['r2network'] = df['r2name'].str.extract(r'\.[^_]*_(?:[^_]*_){1}([^_]+)_')
 
+    # region is after last underscore
     df['r1region'] = df['r1name'].str.extract(r'(?:[^_]*_){3}(.*)')
     df['r2region'] = df['r2name'].str.extract(r'(?:[^_]*_){3}(.*)')
 
+    # handle schaefer400 with different format
     condition = df['r1atlas'] == 'Schaefer400'
     extracted_values = df.loc[condition, 'r1name'].str.extract(r'(?:[^_]*_){2}([^_]+)_')
     df.loc[condition, 'r1hemi'] = extracted_values[0]
@@ -33,6 +38,7 @@ def get_names(df):
     extracted_values = df.loc[condition, 'r2name'].str.extract(r'(?:[^_]*_){4}(.*)')
     df.loc[condition, 'r2region'] = extracted_values[0]
 
+    # handle CONN toolbox networks atlas format
     condition = df['r1atlas'] == 'networks'
     extracted_values = df.loc[condition, 'r1name'].str.extract(r'\.([^\.]*)\.')
     df.loc[condition, 'r1network'] = extracted_values[0]
@@ -40,6 +46,7 @@ def get_names(df):
     extracted_values = df.loc[condition, 'r2name'].str.extract(r'\.([^\.]*)\.')
     df.loc[condition, 'r2network'] = extracted_values[0]
 
+    # handle those with hemisphere in name
     condition = df.r1atlas.str.endswith('_lh')
     df.loc[condition, 'r1hemi'] = 'LH'
     condition = df.r1atlas.str.endswith('_rh')
@@ -50,6 +57,7 @@ def get_names(df):
     condition = df.r2atlas.str.endswith('_rh')
     df.loc[condition, 'r2hemi'] = 'RH'
 
+    # Fill in the rest
     condition = df.r1hemi.isna()
     df.loc[condition, 'r1hemi'] = 'XH'
 
@@ -93,7 +101,7 @@ def get_names(df):
     return df
 
 
-# Read, add columns, write
+# Read, add columns, overwrite
 df = pd.read_csv(ZFILE)
 df = get_names(df)
 df.to_csv(ZFILE, index=False)
