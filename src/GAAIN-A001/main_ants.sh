74a69ae0fea171d5a3c0b265925e8b28ddce6a2a
j-russell-34 <jason.russell34@gmail.com>
2025-02-11 13:33:51 -0800

    Removing non spm8 in spm12 based analyses in GAAIN-A001

--- src/GAAIN-A001/main_ants.sh
+++
@@ -1,3 +0,0 @@
-
-python /Users/jasonrussell/Documents/code/analyses/src/GAAIN-A001/centiloid_suvr_extraction.py
-python /Users/jasonrussell/Documents/code/analyses/src/GAAIN-A001/PiB_validation_nonspm.py
--- src/GAAIN-A001/main_spm12.sh
+++
@@ -1,3 +0,0 @@
-
-python /Users/jasonrussell/Documents/code/analyses/src/GAAIN-A001/suvr_spm12.py
-python /Users/jasonrussell/Documents/code/analyses/src/GAAIN-A001/PiB_validation_spm12.py
