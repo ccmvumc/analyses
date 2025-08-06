624aee21dec0c6bf44f5cc965344b2ba20269ad2
Brian D. Boyd <bud42@users.noreply.github.com>
2025-08-06 13:20:46 -0400

    load covars in task project-level

--- src/CONN/task/merge-main.sh
+++ src/CONN/task/merge-main.sh
@@ -21,3 +21,33 @@ for i in */;do
 done
 
 echo "ALL DONE!"
+
+# covars
+if [ -f "/OUTPUTS/covariates.mat" ]; then
+	echo "Loading covariates to CONN project"
+	xvfb-run \
+	-e /OUTPUTS/xvfb.err -f /OUTPUTS/xvfb.auth \
+	-a --server-args "-screen 0 1600x1200x24" \
+	/opt/conn/run_conn.sh \
+	/opt/mcr/v912 batch \
+	/REPO/src/CONN/load_covariates.m
+
+	rm /OUTPUTS/xvfb.*
+
+	# Run the results
+	#echo "Running 2nd level contrasts in CONN"
+	#xvfb-run \
+	#-e /OUTPUTS/xvfb.err -f /OUTPUTS/xvfb.auth \
+	#-a --server-args "-screen 0 1600x1200x24" \
+	#/opt/conn/run_conn.sh \
+	#/opt/mcr/v912 batch \
+	#/REPO/src/CONN/second_level.m
+
+	#rm /OUTPUTS/xvfb.*
+
+else
+	echo "File not found /OUTPUTS/covariates.mat"
+fi
+
+
+echo "ALL DONE!"
