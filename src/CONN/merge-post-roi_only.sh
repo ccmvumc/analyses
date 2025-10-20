3fbecf2970f43f36a90c09ca438331d47a7d7354
Brian D. Boyd <bud42@users.noreply.github.com>
2025-10-20 17:43:44 -0400

    Update merge-post-roi_only.sh

--- src/CONN/merge-post-roi_only.sh
+++ src/CONN/merge-post-roi_only.sh
@@ -1,13 +1,3 @@
-rm /OUTPUTS/xvfb.auth
-rm /OUTPUTS/xvfb.err
-
-# Delete merged pre-processed files since we only want 1st Level Results
-rm -r /OUTPUTS/conn/results/preprocessing/[A-Za-z]*.mat
-rm -r /OUTPUTS/conn/results/preprocessing/*.nii
-rm -r /OUTPUTS/conn/results/preprocessing/*.matc
-rm -r /OUTPUTS/conn/data/*.mat
-rm -r /OUTPUTS/conn/data/*.matc
-
 # Extract Z matrix to csv with row per value
 python /REPO/src/CONN/zvalues_mat2csv.py
 
