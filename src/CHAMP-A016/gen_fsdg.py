be855832a225daa775c581a4151d49abb09f4154
j-russell-34 <jason.russell34@gmail.com>
2026-01-13 12:33:24 -0800

    change fsdg gen to use str()

--- src/CHAMP-A016/gen_fsdg.py
+++ src/CHAMP-A016/gen_fsdg.py
@@ -12,9 +12,9 @@ def write_fsgd(df, output_file, title):
         
         # Write input lines for each subject skipping missing activation values
         for _, row in df.iterrows():
-            subject = row['SUBJECT'].astype(str)
+            subject = str(row['SUBJECT'])
             age = row['Age']
-            site = row['site'].astype(str)
+            site = str(row['site'])
             #if value in group is 0 write group 1 if value is 1 write group 2
             group = "Group 1" if row['Group'] == 0 else "Group 2"
             f.write(f"Input {subject} {group} {age} {site}\n")
